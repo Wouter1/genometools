a43c73a377caf8a73edd09ed5b60ccd90e209398
Wouter <nomail@hotmail.com>
2026-02-12 15:46:39 +0100

    remove and ignore generated.

--- src/test/scala/abeel/genometools/GenomeToolsE2E.scala
+++ src/test/scala/abeel/genometools/GenomeToolsE2E.scala
@@ -2,15 +2,19 @@ package abeel.genometools;
 
 import org.scalatest.funsuite.*
 import scala.collection.mutable.BitSet
+import io.Source
 
-
-class TestBitSetTools extends AnyFunSuite {
+class GenomeToolsE2E extends AnyFunSuite {
   val DIR = "src/test/resources/"
   val BAM = DIR+"uwgs-rw_L2_FC6218_3.CHROMOSOME_IV.sorted.bam"
-  
+  val STATS="stats"
 
   test("stats") {
-    GenomeToolsConsole.main(Array("bam2gc", BAM))
+    GenomeToolsConsole.main(Array("bam2gc","-i", BAM,"-o",STATS))
+    val lines = Source.fromFile(STATS).mkString
+    assert(lines.size > 10)
+    assert(lines.contains("97\t2")) // whatever that means
+    // CHECK the output contains "Discarded reads = 350458"
   }
 
 }
